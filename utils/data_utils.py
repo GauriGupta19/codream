9fa44b43ebdf236730fee8067c4b83c5a8acf1e9
Gauri Gupta <gaurivgupta19@gmail.com>
2023-04-25 20:34:02 +0000

    add non-iid

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -16,7 +16,7 @@ class CIFAR10_DSET():
         self.num_channels = 3
         self.gen_transform = T.Compose(
             [
-                T.RandomCrop(32, padding=4),
+                T.RandomCrop(size=[32, 32], padding=4),
                 T.RandomHorizontalFlip(),
                 T.Normalize(
                     self.mean, 
@@ -108,68 +108,83 @@ def get_dataset(dname, dpath):
                                                                         rate_unbalance)
     return obj"""
 
-def extr_noniid(train_dataset, samples_per_client, classes):
+
+def non_iid_unbalanced_dataidx_map(dset_obj, n_parties, beta=0.4):
+    train_dset = dset_obj.train_dset
+    n_classes = dset_obj.NUM_CLS
+    
+    N = len(train_dset)
+    labels = np.array(train_dset.targets)
+    min_size = 0
+    min_require_size = 10
+    while min_size < min_require_size:
+        idx_batch = [[] for _ in range(n_parties)]
+        for k in range(n_classes):
+            idx_k = np.where(labels == k)[0]
+            np.random.shuffle(idx_k)
+            proportions = np.random.dirichlet(np.repeat(beta, n_parties))
+            ## Balance
+            proportions = np.array([p * (len(idx_j) < N / n_parties) for p, idx_j in zip(proportions, idx_batch)])
+            proportions = proportions / proportions.sum()
+            proportions = (np.cumsum(proportions) * len(idx_k)).astype(int)[:-1]
+            idx_batch = [idx_j + idx.tolist() for idx_j, idx in zip(idx_batch, np.split(idx_k, proportions))]
+            min_size = min([len(idx_j) for idx_j in idx_batch])
+
+    net_dataidx_map = {}        
+    for j in range(n_parties):
+        np.random.shuffle(idx_batch[j])
+        net_dataidx_map[j] = idx_batch[j]
+    return net_dataidx_map
+
+        
+def non_iid_balanced(dset_obj, n_client, n_data_per_clnt, alpha=0.4):
+    
+    trn_y = np.array(dset_obj.train_dset.targets)
+    trn_x = np.array(dset_obj.train_dset.data)
+    n_cls = dset_obj.NUM_CLS
+    height = width = dset_obj.IMAGE_SIZE
+    channels = dset_obj.num_channels
+
+    clnt_data_list = (np.ones(n_client) * n_data_per_clnt).astype(int)
+    cls_priors   = np.random.dirichlet(alpha=[alpha]*n_cls,size=n_client)
+    prior_cumsum = np.cumsum(cls_priors, axis=1)
+    idx_list = [np.where(trn_y==i)[0] for i in range(n_cls)]
+    cls_amount = [len(idx_list[i]) for i in range(n_cls)]
+
+    clnt_x = [ np.zeros((clnt_data_list[clnt__], height, width, channels)).astype(np.float32) for clnt__ in range(n_client) ]
+    clnt_y = [ np.zeros((clnt_data_list[clnt__], 1)).astype(np.int64) for clnt__ in range(n_client) ]
+
+    while(np.sum(clnt_data_list)!=0):
+        curr_clnt = np.random.randint(n_client)
+        # If current node is full resample a client
+        # print('Remaining Data: %d' %np.sum(clnt_data_list))
+        if clnt_data_list[curr_clnt] <= 0:
+            continue
+        clnt_data_list[curr_clnt] -= 1
+        curr_prior = prior_cumsum[curr_clnt]
+        while True:
+            cls_label = np.argmax(np.random.uniform() <= curr_prior)
+            # Redraw class label if trn_y is out of that class
+            if cls_amount[cls_label] <= 0:
+                continue
+            cls_amount[cls_label] -= 1
+            clnt_x[curr_clnt][clnt_data_list[curr_clnt]] = trn_x[idx_list[cls_label][cls_amount[cls_label]]]
+            clnt_y[curr_clnt][clnt_data_list[curr_clnt]] = trn_y[idx_list[cls_label][cls_amount[cls_label]]]
+
+            break
+
+    clnt_x = np.asarray(clnt_x)
+    clnt_y = np.asarray(clnt_y)
+    
+    return clnt_x, clnt_y
+
+    
+    
+def non_iid_labels(train_dataset, samples_per_client, classes):
     print(classes)
     all_data=Subset(train_dataset,[i for i,(x, y) in enumerate(train_dataset) if y in classes])
     perm=torch.randperm(len(all_data))
     return Subset(all_data,perm[:samples_per_client])
 
-def cifar_extr_noniid(train_dataset, test_dataset, num_users, n_class, num_samples, rate_unbalance):
-    num_shards_train, num_imgs_train = int(50000/num_samples), num_samples
-    num_classes = 10
-    num_imgs_perc_test, num_imgs_test_total = 1000, 10000
-    assert(n_class * num_users <= num_shards_train)
-    assert(n_class <= num_classes)
-    idx_class = [i for i in range(num_classes)]
-    idx_shard = [i for i in range(num_shards_train)]
-    dict_users_train = {i: np.array([]) for i in range(num_users)}
-    dict_users_test = {i: np.array([]) for i in range(num_users)}
-    idxs = np.arange(num_shards_train*num_imgs_train)
-    # labels = dataset.train_labels.numpy()
-    labels = np.array(train_dataset.targets)
-    idxs_test = np.arange(num_imgs_test_total)
-    labels_test = np.array(test_dataset.targets)
-    #labels_test_raw = np.array(test_dataset.targets)
-
-    # stores the image idxs with their corresponding labels
-    # array([[    0,     1,     2, ..., 49997, 49998, 49999],
-    #        [    6,     9,     9, ...,     9,     1,     1]])
-    idxs_labels = np.vstack((idxs, labels))
-    # sorts the whole thing based on labels
-    # array([[29513, 16836, 32316, ..., 36910, 21518, 25648],
-    #       [    0,     0,     0, ...,     9,     9,     9]])
-    idxs_labels = idxs_labels[:, idxs_labels[1, :].argsort()]
-    idxs = idxs_labels[0, :]
-    labels = idxs_labels[1, :]
-
-    # Same things as above except that it is test set now
-    idxs_labels_test = np.vstack((idxs_test, labels_test))
-    idxs_labels_test = idxs_labels_test[:, idxs_labels_test[1, :].argsort()]
-    idxs_test = idxs_labels_test[0, :]
-    #print(idxs_labels_test[1, :])
 
 
-    # divide and assign
-    for i in range(num_users):
-        user_labels = np.array([])
-        rand_set = set(np.random.choice(idx_shard, n_class, replace=False))
-        idx_shard = list(set(idx_shard) - rand_set)
-        unbalance_flag = 0
-        for rand in rand_set:
-            if unbalance_flag == 0:
-                dict_users_train[i] = np.concatenate(
-                    (dict_users_train[i], idxs[rand*num_imgs_train:(rand+1)*num_imgs_train]), axis=0)
-                user_labels = np.concatenate((user_labels, labels[rand*num_imgs_train:(rand+1)*num_imgs_train]), axis=0)
-            else:
-                dict_users_train[i] = np.concatenate(
-                    (dict_users_train[i], idxs[rand*num_imgs_train:int((rand+rate_unbalance)*num_imgs_train)]), axis=0)
-                user_labels = np.concatenate((user_labels, labels[rand*num_imgs_train:int((rand+rate_unbalance)*num_imgs_train)]), axis=0)
-            unbalance_flag = 1
-        user_labels_set = set(user_labels)
-        #print(user_labels_set)
-        #print(user_labels)
-        for label in user_labels_set:
-            dict_users_test[i] = np.concatenate((dict_users_test[i], idxs_test[int(label)*num_imgs_perc_test:int(label+1)*num_imgs_perc_test]), axis=0)
-        #print(set(labels_test_raw[dict_users_test[i].astype(int)]))
-
-    return dict_users_train, dict_users_test
