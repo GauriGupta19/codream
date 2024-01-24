53e8af31e2ed7d711de15f1d3b2fb48ad338e783
Alex Dang <alexdang@mit.edu>
2024-01-24 10:15:15 +0000

    trainable parameter only covariates, prints

--- algos/scaffold.py
+++ algos/scaffold.py
@@ -36,10 +36,11 @@ class ScaffoldOptimizer(Optimizer):
 
         for group in self.param_groups:
             # print(group['params'])
+            assert(len(group["params"]) == len(server_controls) == len(client_controls))
             for p, c, ci in zip(group['params'], server_controls.values(), client_controls.values()):
-                if p.grad is None:
-                    print("what is going on?", p.shape, c.shape, ci.shape)
-                    continue
+                # if p.grad is None:
+                #     print("what is going on?", p.shape, c.shape, ci.shape)
+                #     continue
                 dp = p.grad.data - ci.data + c.data
                 p.data.sub_(group['lr'] * dp)
 
@@ -53,106 +54,111 @@ class SCAFFOLDClient(BaseClient):
         self.tag = CommProtocol
         # create a dictionary to store the client controls
         self.c_i = OrderedDict()
-        for n, p in self.model.state_dict().items():
-            self.c_i[n] = torch.zeros_like(p)
+        for (name, param) in self.model.named_parameters():
+            self.c_i[name] = torch.zeros_like(param.data)
         self.optim = ScaffoldOptimizer(self.model.parameters(), lr=self.config["lr_client"], weight_decay=0.0001)
-
+        # self.optim = torch.optim.SGD(self.model.parameters(), lr=self.config["lr_client"], momentum=0.9, weight_decay=0.0001)
     
-    def local_train(self, model, optim, dloader, loss_fn, device, c, c_i):
+    def local_train(self, model: nn.Module, optim, dloader, loss_fn, device, c, c_i):
         """
         Train the model locally
         """
+        param1 = list(filter(lambda a: a[0] == 'param1', model.named_parameters()))[0][1]
         model.train()
         for i, (x, y) in enumerate(dloader):
+
             optim.zero_grad()
             x, y = x.to(device), y.to(device)
             y_hat = model(x)
             # print("Shape of x,y, y_hat: ", x.shape, y.shape, y_hat.shape)
             loss = loss_fn(y_hat, y)
             loss.backward()
+
+            if self.node_id == 1:
+                print(f"\tClient {self.node_id} epoch {i}", 
+                    f"Loss: {'%.3f' % loss.item()}"
+                    )
+                grad = param1.grad.item()
+                step = grad + c['param1'].item() - c_i['param1'].item()
+                print(
+                    "\t\tparam1", '%.4f' % param1.data.item(), 
+                    "grad", '%.4f' % grad, 
+                    "c", '%.4f' % c['param1'].item(),
+                    "c_i", '%.4f' % c_i['param1'].item(),
+                    "step", '%.4f' % step,
+                    "gradient direction?", "yes" if grad*step > 0 else "NO!"
+                    )
+            
             # for param, server_control, client_control in zip(self.model.parameters(), c.values(), c_i.values()):
-            # param.grad += server_control - client_control
+            #     param.grad += server_control - client_control
             # optim.step({k: torch.zeros_like(c1).to(device) for k, c1 in c.items()}, {k: torch.zeros_like(c_i1).to(device) for k, c_i1 in c_i.items()})
             optim.step(c, c_i)
-            if i % (len(dloader) // 4) == 0:
-                test_loss, acc = self.model_utils.test(self.model,
-                                               self._test_loader,
-                                               self.loss_fn,
-                                               self.device)
-                pass
-
-    def get_weights(self) -> Dict[str, Tensor]:
-        """
-        Share the model weights
-        """
-        return self.model.state_dict()
-
-    def set_weights(self, weights: OrderedDict[str, Tensor]):
-        """
-        Set the model weights
-        """
-        self.model.load_state_dict(weights)
+            # optim.step()
 
     def run_protocol(self):
         start_epochs = self.config.get("start_epochs", 0)
         total_epochs = self.config["epochs"]
+
         for round in range(start_epochs, total_epochs):
             x, c = self.comm_utils.wait_for_signal(src=self.server_node, tag=self.tag.START)
-            # print(f"\tClient {self.node_id}", 
-            #       "\n\t\treceived x", '%.6f' % x['param1'].item(),
-            #       "\n\t\treceived c", '%.6f' % c['param1'].item()
-            # )
-
+            assert(list(c.keys()) == list(self.c_i.keys()))
             # put x and c on the device
             for k, v in x.items():
                 x[k] = v.to(self.device)
             for k, v in c.items():
                 c[k] = v.to(self.device)
+            
             y_i = copy.deepcopy(x)
             self.model.load_state_dict(y_i)
+
             for i in range(self.config["local_runs"]):
-                new_c, new_c_i = OrderedDict(), OrderedDict()
-                for k, v in self.model.named_parameters():
-                    new_c[k] = c[k]
-                    new_c_i[k] = self.c_i[k]
-                self.local_train(self.model, self.optim, self.dloader, self.loss_fn, self.device, new_c, new_c_i)
-            test_loss, acc = self.model_utils.test(self.model,
-                                               self._test_loader,
-                                               self.loss_fn,
-                                               self.device)
-            print(f"\tClient {self.node_id} round {round}", 
-                  f"Loss: {test_loss}",
-                  f"Acc: {acc}",
-                  )
+                # new_c, new_c_i = OrderedDict(), OrderedDict()
+                # for k, v in self.model.named_parameters():
+                #     new_c[k] = c[k]
+                #     new_c_i[k] = self.c_i[k]
+                self.local_train(self.model, self.optim, self.dloader, self.loss_fn, self.device, c, self.c_i)
+            
             # for every parameter in the model, compute the local pseudo gradient
             # and update the client control
             K = len(self.dloader)
-            local_pseudo_grad, c_i_plus, c_i_delta = OrderedDict(), OrderedDict(), OrderedDict()
+
+            local_pseudo_grad = OrderedDict()
             for k, v in self.model.state_dict().items():
                 local_pseudo_grad[k] = v - x[k]
+
+            c_i_plus, c_i_delta = OrderedDict(), OrderedDict()
+            for k in self.c_i.keys():
                 c_i_plus[k] = c[k] - self.c_i[k] + (1 / (K * self.config["lr_client"])) * (-1 * local_pseudo_grad[k])
                 c_i_delta[k] = c_i_plus[k] - self.c_i[k]
+
+            if self.node_id == 1:
+                print("pseudo grad", '%.4f' % local_pseudo_grad['param1'].item())
+
+                
+            # for k, v in self.model.state_dict().items():
+            #     local_pseudo_grad[k] = v - x[k]
+            #     c_i_plus[k] = c[k] - self.c_i[k] + (1 / (K * self.config["lr_client"])) * (-1 * local_pseudo_grad[k])
+            #     c_i_delta[k] = c_i_plus[k] - self.c_i[k]
             
             # print (f"\tClient {self.node_id}", "\t\t After Training")
             # print(f"\tClient {self.node_id}", "\t\t", '%.6f' % c_i_plus['param1'].item(), "c_i_plus")
             # print(f"\tClient {self.node_id}", "\t\t", '%.6f' % self.model.state_dict()['param1'].item(), "y")
 
-            # test_loss, acc = self.model_utils.test(self.model,
-            #                                    self._test_loader,
-            #                                    self.loss_fn,
-            #                                    self.device)
-            # print(f"\tClient {self.node_id}", 
-            #       "\n\t\tc_i_plus", '%.6f' % c_i_plus['param1'].item(), 
-            #       "\n\t\tc_i_delta", '%.6f' % c_i_delta['param1'].item(), 
-            #       "\n\t\ty_i", '%.6f' % self.model.state_dict()['param1'].item(),
-            #       "\n\t\ttest loss", test_loss, 
-            #       "\n\t\tacc", acc)
+            test_loss, acc = self.model_utils.test(self.model,
+                                               self._test_loader,
+                                               self.loss_fn,
+                                               self.device)
+            if self.node_id == 1:
+                print(f"\tClient {self.node_id}", 
+                    # "\n\t\tc_i_plus", '%.6f' % c_i_plus['param1'].item(), 
+                    # "\n\t\tc_i_delta", '%.6f' % c_i_delta['param1'].item(), 
+                    # "\n\t\ty_i", '%.6f' % self.model.state_dict()['param1'].item(),
+                    "\n\t\ttest loss", test_loss, 
+                    "\n\t\tacc", acc)
                   
             self.comm_utils.send_signal(dest=self.server_node, data=local_pseudo_grad, tag=self.tag.WTS)
             self.comm_utils.send_signal(dest=self.server_node, data=c_i_delta, tag=self.tag.COV)
             self.c_i = c_i_plus
-
-# TODO: test smaller model, test degenerate to fedavg, test other implementations with resnet, look over implementations thoroughly
 
 class SCAFFOLDServer(BaseServer):
     def __init__(self, config) -> None:
@@ -161,11 +167,8 @@ class SCAFFOLDServer(BaseServer):
         self.config = config
         self.set_model_parameters(config)
         self.c = OrderedDict()
-        for n, p in self.model.state_dict().items():
-            self.c[n] = torch.zeros_like(p)
-
-        # print(self.c.keys()) 'lin2.weight'
-        # raise Exception("stop here")
+        for (name, param) in self.model.named_parameters():
+            self.c[name] = torch.zeros_like(param.data)
         
         self.tag = CommProtocol
         self.model_save_path = "{}/saved_models/node_{}.pt".format(self.config["results_path"],
@@ -212,8 +215,6 @@ class SCAFFOLDServer(BaseServer):
         for k, v in self.model.state_dict().items():
             state_dict[k] = v + self.config["lr_server"] * delta_x[k]
 
-        # print("\tTEST WEIGHT", torch.linalg.norm(delta_x['lin2.weight']).item())
-        # print("\tMODEL UPDATED?", torch.linalg.norm(self.model.state_dict()['lin2.weight']).item())
         self.model.load_state_dict(state_dict)
         # print('%.6f' % self.model.state_dict()['param1'].item(), "\tSERVER WEIGHT NEW")
 
@@ -233,8 +234,8 @@ class SCAFFOLDServer(BaseServer):
         Get the covariates
         """
         c_temp = OrderedDict()
-        for k, _ in self.model.state_dict().items():
-            c_temp[k] = copy.deepcopy(self.c[k]).to("cpu")
+        for k, v in self.c.items():
+            c_temp[k] = copy.deepcopy(v).to("cpu")
         return c_temp
     
     def get_weights(self) -> Dict[str, Tensor]:
--- configs/iid_clients.py
+++ configs/iid_clients.py
@@ -332,12 +332,12 @@ scaffold = {
     "dump_dir": "./expt_dump/",
     "dpath": "./imgs/cifar10",
     "seed": 1,
-    "device_ids": {"node_0": [3], "node_1": [1], "node_2": [3],},
+    "device_ids": {"node_0": [2], "node_1": [2], "node_2": [3]},
     # Learning setup
-    "num_clients": 2, "samples_per_client": 2000,
-    "local_runs": 5,
-    "epochs": 1000, "model": "resnet18",
-    "lr_client": 0.1, "batch_size": 256,
+    "num_clients": 2, "samples_per_client": 1024,
+    "local_runs": 1,
+    "epochs": 1000, "model": "toy",
+    "lr_client": 0.05, "batch_size": 256,
     "lr_server": 1.,
     "model_lr": 0.1, # decoy parameter not used in scaffold
     "exp_keys": []
